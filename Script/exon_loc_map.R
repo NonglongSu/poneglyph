7b1240c57e540c33263c6cc6d436447bcb295699
ZIQI ZHU <zzhu49@asu.edu>
2020-04-23 17:59:15 -0700

    fix negative values

--- Script/exon_loc_map.R
+++ Script/exon_loc_map.R
@@ -1,7 +1,7 @@
 #We can start make user-interaction
 #Extract the IDs and locations of exons from any random reference gene that we select.
 
-# setwd("~/Dropbox (ASU)/poneglyph/Script")
+ setwd("~/Dropbox (ASU)/poneglyph/Script")
 
 library(methods)
 library(httr)
@@ -12,10 +12,11 @@ library(Biostrings)
 library(BiocGenerics) 
 library(dplyr)
 library(purrr) # accumulate
+library(doParallel)
 
 Find_id_loc = function(ETable, TRANS.ID, EXON.ID) {
   Exon.loc = list()   #Relative location  
-  Exon.id = list()   #Functional Id 
+  Exon.id = list()    #Functional Id 
   server = "http://rest.ensembl.org"
   for(i in 1:length(TRANS.ID)){
     name = gsub('\"', "", TRANS.ID[i], fixed = TRUE)
@@ -30,33 +31,31 @@ Find_id_loc = function(ETable, TRANS.ID, EXON.ID) {
     #extract exon location
     id_exon = id$Exon
     exon.df = do.call(rbind,Map(data.frame, start = id_exon$start, end = id_exon$end))
-    #5'3'UTR removal
-    UTR.5 = which(exon.df$start <= cds.box[1])
-    UTR.3 = which(exon.df$end >= cds.box[2])
-    #locate the translation start/end
-    if (exon.df$start[head(UTR.3,1)] <= cds.box[1] && exon.df$end[tail(UTR.5,1)] >= cds.box[2]) {
-      #translation start inside the last exon
+    exon.df = exon.df[order(exon.df$start), ]
+    #5'3'UTR locus.
+    UTR.5 = which(cds.box[1] >= exon.df$start)
+    UTR.3 = which(cds.box[2] <= exon.df$end)
+    #Find the translation start & end locus.
+    if (exon.df$start[head(UTR.3, 1)] <= cds.box[1] && exon.df$end[tail(UTR.5, 1)] >= cds.box[2]) { #translation start inside the last exon
       cds.start = NULL
       cds.end   = cds.box[2] - cds.box[1] + 1 
-    } else { 
-      #translation start before the last exon
+    } else { #translation start before the last exon
       cds.start = exon.df$end[tail(UTR.5, 1)] - cds.box[1] + 1 
       cds.end   = cds.box[2] - exon.df$start[head(UTR.3, 1)] + 1 
     }
-    
-    #find exon location
+    #find the middle exon locus.
     exon.fix.df  = exon.df[-c(UTR.5,UTR.3),]
     middle.len   = exon.fix.df$end - exon.fix.df$start + 1
     exon.loc     = c(cds.start, c(middle.len,cds.end)) %>% accumulate(`+`)
     Exon.loc[[i]]= exon.loc  
     #filter the exon id
-    exon.id.tmp    = EXON.ID[which(ETable$Transcript == TRANS.ID[i])]
-    if (length(UTR.5) + length(UTR.3) <= 2){
+    exon.id.tmp  = EXON.ID[which(ETable$Transcript == TRANS.ID[i])]
+    if (length(UTR.5) + length(UTR.3) <= 2) { #all in
       exonId.final = exon.id.tmp
-    } else {
+    }else { #partial
       exonId.final = exon.id.tmp[-c(head(UTR.5, -1), tail(UTR.3, -1))]
     }
-    Exon.id[[i]]   = exonId.final
+    Exon.id[[i]] = exonId.final
   }
   Exon.ID = do.call(rbind,Map(data.frame, Transcript = TRANS.ID, Exon = Exon.id))
   res = list(Exon.ID, Exon.loc)
@@ -64,9 +63,9 @@ Find_id_loc = function(ETable, TRANS.ID, EXON.ID) {
 }
 
 
-# inDir  = "../Data/Exon_table/"
-# inFile = "../Data/geneId_cut.txt"
-# ouDir  = "../Data/Results/From/"
+inDir  = "../Data/Exon_table/"
+inFile = "../Data/geneId_cut.txt"
+ouDir  = "../Data/Results/From/"
 
 main = function(inDir, inFile, ouDir){
   geneList   = read_delim(inFile,"\t", col_names = FALSE)
@@ -76,6 +75,8 @@ main = function(inDir, inFile, ouDir){
   #gene.stem = geneList[1971,]
   rand = sample(nrow(geneList), 1, replace = FALSE)
   gene.stem  = geneList[rand,]
+  # Test
+  gene.stem = c("ENSG00000090924", "ENSPTRG00000010969")
   
   ID.LIST = list()
   for (i in 1:length(gene.stem)) {
@@ -90,20 +91,19 @@ main = function(inDir, inFile, ouDir){
   numCores <- detectCores()
   registerDoParallel(numCores)
 
-  foreach (j in 1:length(ID.LIST)) {
+  foreach (j = 1:length(ID.LIST)) %dopar% {
     write.table(ID.LIST[[j]][[1]],
-                paste0(ouDir,name[[j]], "_exonId.txt"),
+                paste0(ouDir,name[j], "_exonId.txt"),
                 quote = FALSE,
                 row.names = FALSE, 
                 col.names = TRUE, 
                 append = FALSE, 
                 sep = "\t")
-    mclapply(ID.LIST[[j]][[2]], 
+    lapply(ID.LIST[[j]][[2]], 
            write,
-           paste0(ouDir,name[[j]], "_exonLoc.txt"),
+           paste0(ouDir, name[j], "_exonLoc.txt"),
            append=TRUE,
-           ncolumns=1000,
-           mc.cores = numCores)
+           ncolumns = 1000)
   }
 
   cat(unlist(gene.stem), file = paste0(ouDir,"geneId.txt"), sep = "\t")
